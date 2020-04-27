ab5e603d50e7fbe6b1b588984f2f94385c99e403
Roland Rickborn <exensio-rori@users.noreply.github.com>
2020-04-27 17:32:44 +0200

    Adapted for Brightcove requirements

--- bin/webvtt.js
+++ bin/webvtt.js
@@ -2,6 +2,7 @@
 
 var parser = require( "../lib/parser" ),
     fs = require( "fs" ),
+    path = require('path'),
     program = require( "commander" );
 
 program
@@ -26,6 +27,18 @@ filenames.forEach( function( filename ) {
       console.log( "webvtt: %s: Not a file.", filename );
       failed = true;
       return;
+    }
+    const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+    var A_Z = range(65, 90, 1); // A-Z
+    var zero_nine = range(48, 57, 1); // 0-9
+    var specialchars = [45, 95]; //'-', '_'
+    var filebasename = path.basename(filename, path.extname(filename));
+    for (var i = 0; i < filebasename.toUpperCase().length; i++) {
+      if ( !(A_Z.concat(zero_nine, specialchars).includes(filebasename.toUpperCase()[i].charCodeAt(0))) ) {
+        console.log( "webvtt: %s: Illegal characters in filename.", filename );
+        failed = true;
+        return;
+      }
     }
   } catch( e ) {
     console.log( "webvtt: %s: No such file.", filename );
@@ -52,7 +65,7 @@ filenames.forEach( function( filename ) {
     error = errors[ i ];
     if( !program.silent ) {
       // sourcefile:lineno.column: message
-      console.log( "%s:%s.%s: %s", filename, error.line, error.col|0, error.message );
+      console.log( "%s: Line %s, Col %s: %s", filename, error.line, error.col|0, error.message );
     }
   }
 });
